d91d16749790cd1fa17ff291c95ca11ce3c27f3e
Hervé Pagès <hpages.on.github@gmail.com>
2024-03-19 23:05:49 -0700

    small enhancement to UCSC.api.url()

--- R/UCSC.api.url.R
+++ R/UCSC.api.url.R
@@ -5,7 +5,18 @@
 
 
 ### Exported.
-UCSC.api.url <- function() getOption("UCSC.api.url")
+UCSC.api.url <- function(new_url=NULL)
+{
+    ans <- getOption("UCSC.api.url")
+    if (is.null(new_url))
+        return(ans)
+    if (!(isSingleString(new_url) && nzchar(new_url)))
+        stop(wmsg("'new_url' must be a single (non-empty) string"))
+    if (!startsWith(tolower(new_url), "http"))
+        stop(wmsg("'new_url' must start with \"http\""))
+    options(UCSC.api.url=new_url)
+    invisible(ans)  # return old URL invisibly
+}
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,6 @@
     ## - Europe: https://genome-euro.ucsc.edu/cgi-bin/hubApi
     ## - Asia: https://genome-asia.ucsc.edu/cgi-bin/hubApi
     ## - Mirror installation: https://your.server.edu/cgi-bin/hubApi
-    url <- "https://api.genome.ucsc.edu"  # primary URL (West Coast)
-    options(UCSC.api.url=url)
+    UCSC.api.url("https://api.genome.ucsc.edu")  # primary URL (West Coast)
 }
 
